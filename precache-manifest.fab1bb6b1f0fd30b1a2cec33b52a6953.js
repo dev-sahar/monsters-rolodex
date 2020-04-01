7811204656c4ea8ec0b35e5e3c93a69948fbcd40
Sahar S <dev_sahar@hotmail.com>
2020-04-01 16:55:14 +0800

    Updates

---
+++ precache-manifest.fab1bb6b1f0fd30b1a2cec33b52a6953.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d0d58f04f2af9c4c13b9aa0a7726fd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ba3458e4e73db8cc5f5",
+    "url": "/static/css/main.8a0294c5.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ba3458e4e73db8cc5f5",
+    "url": "/static/js/main.7d51c21d.chunk.js"
+  },
+  {
+    "revision": "4708b13b98379c58638d",
+    "url": "/static/js/runtime-main.03d56121.js"
+  }
+]);
